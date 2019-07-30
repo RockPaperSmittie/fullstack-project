c23562182ec4ca9b37ce3e2b8f6f5a860c39bab7
Jaco Basson <jakebasson@gmail.com>
2019-07-30 19:33:39 +0200

    Change hardcoded console message to use port

--- src/app.ts
+++ src/app.ts
@@ -24,5 +24,5 @@ const app = new Koa()
 server.applyMiddleware({ app })
 
 app.listen({ port }, () => {
-  console.log(`🚀 Server ready at http://localhost:3000${server.graphqlPath}`)
+  console.log(`🚀 Server ready at http://localhost:${port}${server.graphqlPath}`)
 })
